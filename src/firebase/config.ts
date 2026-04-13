5eed9d82b2c684c4e65f66582305ce0630bed170
Vicetee <viceteecole@gmail.com>
2026-04-13 14:22:25 +0100

    check

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,24 +4,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from "@env";
-
-const API_KEY = import.meta.env.API_KEY;
-const AUTH_DOMAIN = import.meta.env.AUTH_DOMAIN;
-const PROJECT_ID = import.meta.env.PROJECT_ID;
-const STORAGE_BUCKET = import.meta.env.STORAGE_BUCKET;
-const MESSAGING_SENDER_ID = import.meta.env.MESSAGING_SENDER_ID;
-const APP_ID = import.meta.env.APP_ID;
-const MEASUREMENT_ID = import.meta.env.MEASUREMENT_ID;
-
+  
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASUREMENT_ID
+  apiKey: "AIzaSyBpEKGsYrsgXa4p_0F7BLX3DUs_TqSDnTM",
+  authDomain: "my-guy-dd4f7.firebaseapp.com",
+  projectId: "my-guy-dd4f7",
+  storageBucket: "my-guy-dd4f7.firebasestorage.app",
+  messagingSenderId: "517693817597",
+  appId: "1:517693817597:web:a6994f6958d70b0712f494",
+  measurementId: "G-QXWR8WJE9Y"
 };
 
 // Initialize Firebase

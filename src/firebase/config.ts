a9c64058aa473b9cf4761f4cc956709663675370
Vicetee <viceteecole@gmail.com>
2026-04-14 17:29:12 +0100

    dew

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 
   
 const firebaseConfig = {
-  apiKey: import.meta.env.API_KEY,
-  authDomain:  import.meta.env.AUTH_DOMAIN,
-  projectId:   import.meta.env.PROJECT_ID,
-  storageBucket: import.meta.env.STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
-  appId: import.meta.env.APP_ID,
-  measurementId: import.meta.env.MEASUREMENT_ID
+  apiKey: process.env.API_KEY,
+  authDomain:  process.env.AUTH_DOMAIN,
+  projectId:   process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
